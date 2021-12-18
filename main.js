a931140fdb85587916ed58d5894a84a34b0b2319
Leonhelm <qwiray@gmail.com>
2021-12-18 23:14:10 +0300

    Added view battle log

--- main.js
+++ main.js
@@ -1,10 +1,59 @@
 "use strict";
 
+const HIT = {
+  head: getRandom(20, 30),
+  body: getRandom(20, 30),
+  foot: getRandom(20, 30),
+};
+const ATTACK = ["head", "body", "foot"];
+
+const LOGS = {
+  start:
+    "Часы показывали [time], когда [player1] и [player2] бросили вызов друг другу.",
+  end: [
+    "Результат удара [playerWins]: [playerLose] - труп",
+    "[playerLose] погиб от удара бойца [playerWins]",
+    "Результат боя: [playerLose] - жертва, [playerWins] - убийца",
+  ],
+  hit: [
+    "[playerDefence] пытался сконцентрироваться, но [playerKick] разбежавшись раздробил копчиком левое ухо врага.",
+    "[playerDefence] расстроился, как вдруг, неожиданно [playerKick] случайно раздробил грудью грудину противника.",
+    "[playerDefence] зажмурился, а в это время [playerKick], прослезившись, раздробил кулаком пах оппонента.",
+    "[playerDefence] чесал <вырезано цензурой>, и внезапно неустрашимый [playerKick] отчаянно размозжил грудью левый бицепс оппонента.",
+    "[playerDefence] задумался, но внезапно [playerKick] случайно влепил грубый удар копчиком в пояс оппонента.",
+    "[playerDefence] ковырялся в зубах, но [playerKick] проснувшись влепил тяжелый удар пальцем в кадык врага.",
+    "[playerDefence] вспомнил что-то важное, но внезапно [playerKick] зевнув, размозжил открытой ладонью челюсть противника.",
+    "[playerDefence] осмотрелся, и в это время [playerKick] мимоходом раздробил стопой аппендикс соперника.",
+    "[playerDefence] кашлянул, но внезапно [playerKick] показав палец, размозжил пальцем грудь соперника.",
+    "[playerDefence] пытался что-то сказать, а жестокий [playerKick] проснувшись размозжил копчиком левую ногу противника.",
+    "[playerDefence] забылся, как внезапно безумный [playerKick] со скуки, влепил удар коленом в левый бок соперника.",
+    "[playerDefence] поперхнулся, а за это [playerKick] мимоходом раздробил коленом висок врага.",
+    "[playerDefence] расстроился, а в это время наглый [playerKick] пошатнувшись размозжил копчиком губы оппонента.",
+    "[playerDefence] осмотрелся, но внезапно [playerKick] робко размозжил коленом левый глаз противника.",
+    "[playerDefence] осмотрелся, а [playerKick] вломил дробящий удар плечом, пробив блок, куда обычно не бьют оппонента.",
+    "[playerDefence] ковырялся в зубах, как вдруг, неожиданно [playerKick] отчаянно размозжил плечом мышцы пресса оппонента.",
+    "[playerDefence] пришел в себя, и в это время [playerKick] провел разбивающий удар кистью руки, пробив блок, в голень противника.",
+    "[playerDefence] пошатнулся, а в это время [playerKick] хихикая влепил грубый удар открытой ладонью по бедрам врага.",
+  ],
+  defence: [
+    "[playerKick] потерял момент и храбрый [playerDefence] отпрыгнул от удара открытой ладонью в ключицу.",
+    "[playerKick] не контролировал ситуацию, и потому [playerDefence] поставил блок на удар пяткой в правую грудь.",
+    "[playerKick] потерял момент и [playerDefence] поставил блок на удар коленом по селезенке.",
+    "[playerKick] поскользнулся и задумчивый [playerDefence] поставил блок на тычок головой в бровь.",
+    "[playerKick] старался провести удар, но непобедимый [playerDefence] ушел в сторону от удара копчиком прямо в пятку.",
+    "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
+    "[playerKick] не думал о бое, потому расстроенный [playerDefence] отпрыгнул от удара кулаком куда обычно не бьют.",
+    "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
+  ],
+  draw: "Ничья - это тоже победа!",
+};
+
 const $arenas = document.querySelector(".arenas");
 const $controlForm = document.querySelector("form.control");
 const $controlFormSubmitButton = $controlForm.querySelector(
   'button[type="submit"]'
 );
+const $chat = document.querySelector(".chat");
 
 const scorpion = {
   player: 1,
@@ -27,13 +76,6 @@ const subZero = {
   renderHP,
   renderWins,
 };
-
-const HIT = {
-  head: getRandom(20, 30),
-  body: getRandom(20, 30),
-  foot: getRandom(20, 30),
-};
-const ATTACK = ["head", "body", "foot"];
 
 function changeHP(damage) {
   const hpAfter = this.hp - damage;
@@ -59,35 +101,86 @@ $controlForm.addEventListener("submit", function (e) {
   const subZeroAttack = enemyAttack();
   $form.reset();
 
-  if (scorpionAttack.hit !== subZeroAttack.defence) {
+  if (scorpionAttack.hit === subZeroAttack.defence) {
+    generateLogs("defence", scorpion, subZero);
+  } else {
     subZero.changeHP(scorpionAttack.value);
     subZero.renderHP();
+    generateLogs("hit", scorpion, subZero);
   }
 
-  if (subZeroAttack.hit !== scorpionAttack.defence) {
+  if (subZeroAttack.hit === scorpionAttack.defence) {
+    generateLogs("defence", subZero, scorpion);
+  } else {
     scorpion.changeHP(subZeroAttack.value);
     scorpion.renderHP();
+    generateLogs("hit", subZero, scorpion);
   }
 
+  showResult();
+});
+
+$arenas.appendChild(createPlayer(scorpion));
+$arenas.appendChild(createPlayer(subZero));
+generateLogs("start", subZero, scorpion);
+
+function generateLogs(type, player1, player2) {
+  const logText = makeLog(type, player1, player2);
+  const el = `<p>${logText}</p>`;
+  $chat.insertAdjacentHTML("afterbegin", el);
+}
+
+function makeLog(type, player1, player2) {
+  const logType = LOGS[type];
+  const text = Array.isArray(logType)
+    ? logType[getRandom(0, logType.length - 1)]
+    : logType;
+
+  switch (type) {
+    case "start":
+      return text
+        .replace("[time]", formatTime(new Date()))
+        .replace("[player1]", player1.name)
+        .replace("[player2]", player2.name);
+    case "end":
+      return text
+        .replace("[playerWins]", player1.name)
+        .replace("[playerLose]", player2.name);
+    case "hit":
+      return text
+        .replace("[playerKick]", player1.name)
+        .replace("[playerDefence]", player2.name);
+    case "defence":
+      return text
+        .replace("[playerKick]", player1.name)
+        .replace("[playerDefence]", player2.name);
+    case "draw":
+      return text;
+    default:
+      throw new Error('Argument "type" is defined incorrectly');
+  }
+}
+
+function showResult() {
   const isScorpionLose = scorpion.hp <= 0;
   const isSubZeroLose = subZero.hp <= 0;
 
   if (isScorpionLose || isSubZeroLose) {
     if (isScorpionLose && isSubZeroLose) {
       $arenas.appendChild(roundDraw());
+      generateLogs("draw");
     } else if (isScorpionLose) {
       $arenas.appendChild(subZero.renderWins());
+      generateLogs("end", subZero, scorpion);
     } else if (isSubZeroLose) {
       $arenas.appendChild(scorpion.renderWins());
+      generateLogs("end", scorpion, subZero);
     }
 
     disableControlFormSubmitButton();
     $arenas.appendChild(createReloadButton());
   }
-});
-
-$arenas.appendChild(createPlayer(scorpion));
-$arenas.appendChild(createPlayer(subZero));
+}
 
 function playerAttack($form) {
   const playerAttackResult = {};
@@ -173,4 +266,11 @@ function createElement(tagName, className) {
 
 function getRandom(min, max) {
   return Math.round(Math.random() * (max - min) + min);
+}
+
+function formatTime(date) {
+  const hours = `${date.getHours()}`.padStart(2, "0");
+  const minutes = `${date.getMinutes()}`.padStart(2, "0");
+
+  return `${hours}:${minutes}`;
 }
